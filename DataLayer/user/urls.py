635c8b7bd7bb812c664f6126a8b4614269c9916c
huds-on <seanhudson2004@gmail.com>
2024-12-30 20:18:34 +0100

    i modified the views

--- DataLayer/user/urls.py
+++ DataLayer/user/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('profiles/', views.profile_list),
-    path('clinics/', views.clinic_list),
+    # path('clinics/', views.clinic_list),
     # path('profile/<int:id>/', views.profile_detail),
 
     path('register/', UserRegistrationView.as_view(), name='register'),
@@ -13,4 +13,11 @@ urlpatterns = [
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('profiles/', views.ProfileList.as_view()),
+    path('clinics/', views.ClinicList.as_view()),
+    path('health/', views.HealthList.as_view()),
+    path('appointment/', views.AppointmentList.as_view()),
+    path('profile/<int:id>/', views.ProfileDetail.as_view()),
+    path('clinic/<int:id>/', views.ClinicDetail.as_view()),
+    path('health/<int:id>/', views.HealthDetail.as_view()),
 ]
